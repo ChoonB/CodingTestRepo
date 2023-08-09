0ae37e2d90452ce99775227b24067728babab7ad
ChoonB <shgkdc@gmail.com>
2023-08-09 09:30:34 +0900

    [level 2] Title: 구명보트, Time: 9.23 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int count = 0;
+        int left = 0, right = people.length - 1;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            count++;
+        }
+        return count;
+    }
+}
