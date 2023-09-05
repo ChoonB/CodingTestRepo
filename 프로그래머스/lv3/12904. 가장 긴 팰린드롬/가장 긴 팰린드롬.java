1f1b6c1f780cdc2127cb8359d9cc1ec75a121db5
ChoonB <shgkdc@gmail.com>
2023-09-05 09:50:43 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 510.15 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -0,0 +1,24 @@
+class Solution {
+    private boolean isP(int left, int right, String s){
+        
+        while(left<right){
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    public int solution(String s) {
+        int answer = 1;
+        if(s.length() == 1) return 1;
+        
+        for(int i=0; i<s.length()-1; i++){
+            for(int j=s.length()-1; j>0; j--){
+                if(isP(i,j,s)) answer = Math.max(answer, j-i+1);
+            }
+        }
+
+        return answer;
+    }
+}
