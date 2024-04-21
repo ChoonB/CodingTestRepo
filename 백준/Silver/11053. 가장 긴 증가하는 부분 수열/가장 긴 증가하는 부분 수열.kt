22241737e9f56dcccf82650994f6dc57f827a7aa
ChoonB <shgkdc@gmail.com>
2024-04-21 15:43:45 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 192 ms, Memory: 22476 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.kt
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.kt
@@ -1,16 +1,18 @@
 fun main() {
     val n = readln().toInt()
     val nums = readln().split(" ").map { it.toInt() }
-    
+
+    var maxLen = 0
     val dp = IntArray(n) { 1 }
 
-    for (i in 1 until n) {
+    for (i in 0 until n) {
         for (j in 0 until i) {
             if (nums[i] > nums[j]) {
                 dp[i] = maxOf(dp[i], dp[j] + 1)
             }
         }
+        maxLen = maxOf(maxLen, dp[i])
     }
 
-    print(dp.maxOrNull() ?: 0)
+    print(maxLen)
 }
