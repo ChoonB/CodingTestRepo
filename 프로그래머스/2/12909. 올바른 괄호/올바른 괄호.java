92490050878a49633d7258668805a7b9c2e263fd
ChoonB <shgkdc@gmail.com>
2024-07-12 09:23:14 +0900

    [level 2] Title: 올바른 괄호, Time: 19.02 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '('){
+                stack.push('(');
+            } else {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        if (stack.isEmpty()) answer = true;
+        return answer;
+    }
+}
